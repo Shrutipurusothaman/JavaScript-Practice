5c74b8a4dbb707ec3631703c644af8a4121a69fa
Shrutipurusothaman <shrutikishore04@gmail.com>
2026-01-04 19:13:48 +0530

    edit section

--- day-7/script.js
+++ day-7/script.js
@@ -3,6 +3,7 @@ const noteInput = document.getElementById("noteInput");
 const notesList = document.getElementById("notesList");
 const errorMsg = document.getElementById("errorMsg");
 let notes = JSON.parse(localStorage.getItem("notes")) || [];
+let editIndex = null;
 displayNotes();
 noteForm.addEventListener("submit", function (e) {
     e.preventDefault();
@@ -12,16 +13,30 @@ noteForm.addEventListener("submit", function (e) {
         return;
     }
     errorMsg.textContent = "";
-    notes.push(noteText);
+    if (editIndex !== null) {
+        notes[editIndex] = noteText;
+        editIndex = null;
+    } else {
+        notes.push(noteText);
+    }
     localStorage.setItem("notes", JSON.stringify(notes));
     noteInput.value = "";
     displayNotes();
 });
 function displayNotes() {
     notesList.innerHTML = "";
+
     notes.forEach((note, index) => {
         const li = document.createElement("li");
         li.textContent = note;
+        const editBtn = document.createElement("button");
+        editBtn.textContent = "Edit";
+        editBtn.className = "edit";
+        editBtn.addEventListener("click", function () {
+            noteInput.value = note;
+            editIndex = index;
+        });
+
         const deleteBtn = document.createElement("button");
         deleteBtn.textContent = "Delete";
         deleteBtn.className = "delete";
@@ -30,6 +45,8 @@ function displayNotes() {
             localStorage.setItem("notes", JSON.stringify(notes));
             displayNotes();
         });
+
+        li.appendChild(editBtn);
         li.appendChild(deleteBtn);
         notesList.appendChild(li);
     });
